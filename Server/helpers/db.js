da110be43a7a62b04486a5897582927afb84f1b7
BibekKoirala07 <www.koiralabibek2058@gmail.com>
2025-02-17 13:55:26 +0545

    mongo dB error added

--- Server/helpers/db.js
+++ Server/helpers/db.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 exports.connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
-    console.log('MongoDB connected...')
+    });
+    console.log("MongoDB connected...");
   } catch (err) {
-    console.log(err)
+    console.log("MongoDb Not connected!!!");
+    console.log(err);
   }
-}
+};
